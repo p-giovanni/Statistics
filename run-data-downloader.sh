d0e1c27b5c7f5c269686c5b397604d5d847c1958
p-giovanni <rizzardig@gmail.com>
2021-03-08 12:40:16 +0100

    New data

--- run-data-downloader.sh
+++ run-data-downloader.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+usage="Usage: run-data-downloader.sh [data|vac]"
+
 VIRTUALENV=/home/giovanni/add-on/venv/bin/activate
-SCRIPT_PATH=./src/DataDownloader.py
 source ${VIRTUALENV}
 
+DATA_SCRIPT_PATH=./src/DataDownloader.py
+VAC_SCRIPT_PATH=./src/VacciniDownload.py
 
-python ${SCRIPT_PATH} $1 $2 $3 $4
+case $1 in
+data)
+  echo "Download regional COVID19 data."
+  python ${DATA_SCRIPT_PATH} $2 $3 $4 $5
+  ;;
+vac)
+  echo "Download vaccination dataset."
+  python ${VAC_SCRIPT_PATH} --download
+  ;;
+help|h)
+  echo $usage
+  ;;
+*)
+  echo "Download regional COVID19 data."
+  python ${DATA_SCRIPT_PATH} $1 $2 $3 $4
+  ;;
+esac
