f9ad61ea7a0222870ca79e5ff4fb00e5bc9387dd
p-giovanni <rizzardig@gmail.com>
2021-03-21 11:45:34 +0100

    New chart.

--- src/VacciniDownload.py
+++ src/VacciniDownload.py
@@ -31,6 +31,8 @@ from ChartTools import text_box
 
 locale.setlocale(locale.LC_ALL, 'it_IT.UTF-8')
 
+colors = ["#9aff33","#34ff33","#33ff98","#33fffe","#339aff","#3371ff","#5b33ff","#c133ff","#ff33d7"]
+
 def download_csv_file(url:str, data_file:str) -> ResultValue:
     log = logging.getLogger('download_csv_file')
     log.info(" >>")
@@ -115,7 +117,6 @@ def plot_vaccinations_by_time(df:pd.DataFrame, ax:mp.axes.Axes, wich:str="first"
         y_cum_sum = grp_by_time["prima_dose"].cumsum()
 
         set_axes_common_properties(ax, no_grid=False)
-
         ax.get_yaxis().set_major_formatter(mp.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
         remove_tick_lines('x', ax)
@@ -131,7 +132,7 @@ def plot_vaccinations_by_time(df:pd.DataFrame, ax:mp.axes.Axes, wich:str="first"
         ax.set_title("Vaccinazioni nel tempo - prima dose", fontsize=18)
         ax.tick_params(axis='y', colors=ln_one_color)
         ax.yaxis.label.set_color(ln_one_color)
-
+        
         ax.scatter(x, y_cum_sum, color=ln_one_color, s=30, marker='.')
         ln_one = ax.plot(x, y_cum_sum, 'b-', linewidth=2, color=ln_one_color, label="Dosi - somma")
 
@@ -158,6 +159,33 @@ def plot_vaccinations_by_time(df:pd.DataFrame, ax:mp.axes.Axes, wich:str="first"
         return ResultKo(ex)
     log.info(" <<")
     return ResultOk(True)
+
+def plot_delivered_vaccines_quantity(df_delivered:pd.DataFrame, ax:mp.axes.Axes)-> ResultValue :
+    log = logging.getLogger('plot_vaccinations_by_time')
+    log.info(" >>")
+    width = 1.2
+    bar_space = width/3
+    try:
+        df_delivered.sort_values(by="data_consegna", inplace=True)
+        by_date = df_delivered.groupby(["data_consegna"]).sum()
+        by_date.reset_index(level=0, inplace=True)
+              
+        x_del = by_date["data_consegna"]
+        y_del = by_date["numero_dosi"]
+        
+        remove_tick_lines('x', ax)
+        remove_tick_lines('y', ax)
+        set_axes_common_properties(ax, no_grid=True)
+
+        ax.bar(x_del, y_del, color=colors[4], width=width, label='2020')
+        ax.set_xticklabels(x_del, rotation=80)
+
+    except Exception as ex:
+        log.error("Exception caught - {ex}".format(ex=ex))
+        return ResultKo(ex)
+    log.info(" <<")
+    return ResultOk(True)
+
 
 def chart_vaccinations_fornitore(df:pd.DataFrame, ax:mp.axes.Axes)-> ResultValue :
     log = logging.getLogger('chart_vaccinations_fornitore')
@@ -217,7 +245,6 @@ def age_distribution(df:pd.DataFrame, ax:mp.axes.Axes, gender:str="F")-> ResultV
         by_age.reset_index(level=0, inplace=True)
         by_age["totals"] = by_age["sesso_femminile"] + by_age["sesso_maschile"]
 
-        colors = ["#9aff33","#34ff33","#33ff98","#33fffe","#339aff","#3371ff","#5b33ff","#c133ff","#ff33d7"]
         values = by_age["sesso_femminile" if gender == "F" else ("sesso_maschile" if gender == "M" else "totals")]
         labels = by_age["fascia_anagrafica"]
         ax.pie(values, labels=labels,  autopct='%1.1f%%', colors=colors)
@@ -228,7 +255,7 @@ def age_distribution(df:pd.DataFrame, ax:mp.axes.Axes, gender:str="F")-> ResultV
         return ResultKo(ex)
     log.info(" <<")
     return ResultOk(True)
-#
+
 def main( args:argparse.Namespace ) -> ResultValue :
     log = logging.getLogger('Main')
     log.info(" >>")
@@ -279,7 +306,7 @@ def main( args:argparse.Namespace ) -> ResultValue :
             df_region = df.loc[mask_region, ["data_somministrazione", "totali", 'fascia_anagrafica', "sesso_maschile","sesso_femminile", "fornitore", "prima_dose", "seconda_dose"]]
 
             mask_region = (df_delivered["nome_area"] == region_name)
-            df_delivered_region = df_delivered.loc[mask_region, ["fornitore","numero_dosi"]]
+            df_delivered_region = df_delivered.loc[mask_region, ["fornitore","numero_dosi","data_consegna"]]
 
 
             fig = plt.figure(figsize=(20, 10))
@@ -291,7 +318,7 @@ def main( args:argparse.Namespace ) -> ResultValue :
             ax.append(fig.add_subplot(gs1[0,0]))
             idx = 0
                   
-            result = company_distribution(df_delivered_region, ax=ax[idx])
+            result = plot_delivered_vaccines_quantity(df_delivered_region, ax=ax[idx])
                
             plt.savefig(os.path.join(os.sep, "tmp", "vaccini_fig.png")
                                     ,bbox_inches = 'tight'
